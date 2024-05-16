a01cac53db08d391b8cd570a4c0c7f92c152a872
unknown <nurrukhans@gmail.com>
2024-05-16 20:58:15 +0700

    create algorithm to hendle matematic operation

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [numbers, setNumbers] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [result, setResult] = useState('');
+
+  useEffect(() => {
+    const randomNumber = Array.from({ length: 4 }, () => Math.floor(Math.random() * 10) + 1);
+    setNumbers(randomNumber);
+  }, []);
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const expression = inputValue.trim();
+    const evalResult = evaluateExpression(expression);
+    console.log(evalResult); // Menampilkan hasil evaluasi di konsol
+    setResult(evalResult);
+  };
+
+  const evaluateExpression = (expression) => {
+    try {
+      return eval(expression);
+    } catch (error) {
+      console.error('Error evaluating expression:', error);
+      return 'Error';
+    }
+  };
+
+  const handleNumberClick = (value) => {
+    setInputValue(inputValue + value);
+  };
+
+  const handleOperatorClick = (operator) => {
+    setInputValue(inputValue + operator);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-gray-500">
+        <div className='text-white'>
+          <h1 className='text-4xl'>Hasilkan Angka 32</h1>
+          <p>Berdasarkan angka di bawah ini</p>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={inputValue} onChange={handleChange} />
+          <button type="submit" className='bg-green-500 text-white w-20'>Submit</button>
+        </form>
+        <div className="text-white">
+          {numbers.map((number, index) => {
+            return (
+              <button key={index} className='bg-blue-500 w-10' onClick={() => handleNumberClick(number)}>{number}</button>
+            )
+          })}
+        </div>
+        <div className='text-white'>
+          <button className='bg-green-500 w-10' onClick={() => handleOperatorClick('+')}>+</button>
+          <button className='w-10 bg-red-500' onClick={() => handleOperatorClick('-')}>-</button>
+          <button className='w-10 bg-blue-500' onClick={() => handleOperatorClick('*')}>*</button>
+          <button className='w-10 bg-indigo-700' onClick={() => handleOperatorClick('/')}>/</button>
+        </div>
+        <div className="text-white">
+          {result === 32 ? (
+            <p>Hasil: {result}</p>
+          ) : (
+            <p>Hasil {result} bukan hasil yang diinginkan, yang diinginkan adalah 32</p>
+          )}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
-}
 
-export default App
+}
+export default App;
